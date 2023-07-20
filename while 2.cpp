f5c8b44bcd4e52dae616582e8c2f9ac1a52bfcf8
Geetanshikha <84942962+Geetanshikha@users.noreply.github.com>
2023-07-20 17:51:45 +0530

    while cw

---
+++ while 2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=5;
+	while(a>=1)
+	{
+		printf(" %d ",a);
+		a--;
+	}
+	return 0;
+}
---
+++ while 3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	while(a>=1)
+	{
+		printf(" %d ",a);
+		a--;
+	}
+	return 0;
+}
